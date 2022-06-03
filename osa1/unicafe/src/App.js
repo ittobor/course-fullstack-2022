eba3d36f9a4e7d2c9ffbdccd9c9f32941c855ddc
Jussi-Pekka Martikainen <jbmartik@gmail.com>
2022-06-03 21:56:39 +0300

    tehtävä 1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,15 @@
 import { useState } from 'react'
 
+const goodText = 'Good'
+const neutralText = 'Neutral'
+const badText = 'Bad'
+const allText = 'All'
+
+const header1Text = 'Give feedback'
+const header2Text = 'Statistics'
+
+const labelPositive = 'Positive'
+
 const Header = ({text}) => {
   return (
     <h1>{text}</h1>
@@ -22,21 +32,25 @@ const Percentage = ({label, dividend, divisor}) => (
   <div>{label} {(dividend / divisor) * 100} %</div>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <>
+    <Header text={header2Text} />
+    <Count text={goodText} value={good} />
+    <Count text={neutralText} value={neutral} />
+    <Count text={badText} value={bad} />
+    <Count text={allText} value={good+neutral+bad} />
+    <Average good={good} neutral={neutral} bad={bad} />
+    <Percentage label={labelPositive} dividend={good} divisor={(good+neutral+bad)} />
+    </>
+  )
+}
+
 const App = () => {
   // tallentaan napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const goodText = 'Good'
-  const neutralText = 'Neutral'
-  const badText = 'Bad'
-  const allText = 'All'
-
-  const header1Text = 'Give feedback'
-  const header2Text = 'Statistics'
-
-  const labelPositive = 'Positive'
 
   return (
     <div >
@@ -44,13 +58,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text={goodText} />
       <Button handleClick={() => setNeutral(neutral + 1)} text={neutralText} />
       <Button handleClick={() => setBad(bad + 1)} text={badText} />
-      <Header text={header2Text} />
-      <Count text={goodText} value={good} />
-      <Count text={neutralText} value={neutral} />
-      <Count text={badText} value={bad} />
-      <Count text={allText} value={good+neutral+bad} />
-      <Average good={good} neutral={neutral} bad={bad} />
-      <Percentage label={labelPositive} dividend={good} divisor={(good+neutral+bad)} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
