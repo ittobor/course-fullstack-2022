bbfcee14ed72dabf55080d9e48978fb12ebfb0c6
Jussi-Pekka Martikainen <jbmartik@gmail.com>
2022-06-08 23:44:38 +0300

    tehtävä 2.1

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,50 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part part={part} key={part.id}/>)}
+  </>
+
+const Course = ({course}) => (
+    <>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+    </>
+  )
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
