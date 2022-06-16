4806da5acaedfc22eeb2040238b8ac0b8c8c4004
Jussi-Pekka Martikainen <jbmartik@gmail.com>
2022-06-16 22:01:12 +0300

    tehtävä 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,21 +1,22 @@
-import React from 'react'
-import { useState } from 'react'
-import Entry from './components/Entry'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import PersonsOfInterest from './components/PersonsOfInterest'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '+358401231244' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newNameFilter, setNewNameFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })  
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
